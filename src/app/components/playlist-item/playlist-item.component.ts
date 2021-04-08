895e66c9dffbd5b534f0a8b5f1115708a95d9956
Ana Valdemoro <ana_v_r97@hotmail.com>
2021-04-08 17:22:16 +0100

    Adding style to playlist-item-component, and funcionality to thumbs ups icon

--- src/app/components/playlist-item/playlist-item.component.ts
+++ src/app/components/playlist-item/playlist-item.component.ts
@@ -10,7 +10,7 @@ export class PlaylistItemComponent implements OnInit {
   @Input() track: ISong;
   @Input() index:number;
   constructor() { }
-
+  thumbsUpToggleIcon:string="far";
   ngOnInit() {}
 
   public formatTime(milisecs: number):string {
@@ -18,5 +18,15 @@ export class PlaylistItemComponent implements OnInit {
     var minutes = Math.floor( secs / 60) || 0;
     var seconds = (secs - (minutes * 60)) || 0;
     return minutes + ':' + (seconds < 10 ? '0' : '') + seconds;
+  }
+  onlike(){
+    
+    if(this.thumbsUpToggleIcon == "far"){
+      this.thumbsUpToggleIcon = "fas";
+      console.log("le hems dado like");
+    }else{
+      this.thumbsUpToggleIcon = "far";
+      console.log("le hems uitado like");
+    }
   }
 }
