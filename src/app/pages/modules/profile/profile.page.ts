566c5f84d82790470484d27c892a1e5ba858b531
Ana Valdemoro <ana_v_r97@hotmail.com>
2021-04-15 22:17:04 +0100

    Reorganising style, renaming some classes and doing profile page

--- src/app/pages/modules/profile/profile.page.ts
+++ src/app/pages/modules/profile/profile.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IUser } from 'src/app/models/user.model';
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.page.scss'],
 })
 export class ProfilePage implements OnInit {
-
+  editToggleIcon:string = "fa-pen";
+  disableEditionMode: boolean = true;
+  user: IUser = {
+    name: "Dua Lipa",
+    surname: "Smith",
+    email: "dualipa@gmail.ccomm",
+    password: "ANoche"
+  } as IUser;
   constructor() { }
 
   ngOnInit() {
   }
+  onDeleteAccount(){
 
+  }
+  toggleEditionMode(){
+    this.disableEditionMode = !this.disableEditionMode ;
+    if(this.editToggleIcon == "fa-pen"){
+      this.editToggleIcon = "fa-save";
+    }else{
+      this.editToggleIcon = "fa-pen";
+    }
+  }
 }
