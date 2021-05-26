2e062f1301dfabbca275933b4ec6afed9c63738f
Ana Valdemoro <ana_v_r97@hotmail.com>
2021-05-26 10:48:54 +0100

    Delete comments

--- src/app/pages/modules/search/search.page.ts
+++ src/app/pages/modules/search/search.page.ts
@@ -10,41 +10,10 @@ import { FirestoreService } from 'src/app/services/firestore.service';
 export class SearchPage implements OnInit {
    songs:ISong[]; 
    moreSongsExist:boolean = true;
-  //   {
-  //     name: "Baila conmigoooooo",
-  //     artists: ["Selena Gómez", "Rauw Alejandro"],
-  //     duration: "3:04",
-  //     photoURL:"https://pbs.twimg.com/media/Esru2SuWMAM_TD6.jpg:large",
-  //     path:"../../../../assets/tracks/Selena Gomez, Rauw Alejandro - Baila Conmigo (Official Video).m4a",
-  //     timesPlaying: 1458
-  //   },
-  //   {
-  //     name: "Bailaa conmigo",
-  //     artists: ["Selena Gómez", "Rauw Alejandro"],
-  //     duration: "3:04",
-  //     photoURL:"https://pbs.twimg.com/media/Esru2SuWMAM_TD6.jpg:large",
-  //     path:"../../../../assets/tracks/Selena Gomez, Rauw Alejandro - Baila Conmigo (Official Video).m4a",
-  //     timesPlaying: 2589
 
-  //   },
-  //   {
-  //     name: "Bailaa conmigo",
-  //     artists: ["Selena Gómez", "Rauw Alejandro"],
-  //     duration: "3:04",
-  //     photoURL:"https://pbs.twimg.com/media/Esru2SuWMAM_TD6.jpg:large",
-  //     path:"../../../../assets/tracks/Selena Gomez, Rauw Alejandro - Baila Conmigo (Official Video).m4a",
-  //     timesPlaying: 2589
-
-  //   }];
   constructor(private fireStore: FirestoreService) { }
 
   async ngOnInit() {
- 
-    // this.fireStore.getAllSongs().then(querySnapshot =>{
-    //   querySnapshot.forEach((doc)=>{
-    //     console.log(doc.id, "=>", doc.data())
-    //   })
-    // } );
     this.fireStore.getInitSongs().then(songs => this.songs = songs);
     
   }
