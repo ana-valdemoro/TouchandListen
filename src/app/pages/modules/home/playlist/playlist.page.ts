66658998be051904fd8b9572db8b30baf05005f5
Ana Valdemoro <ana_v_r97@hotmail.com>
2021-06-04 12:14:08 +0100

    Playlist page updated with firebase data

--- src/app/pages/modules/home/playlist/playlist.page.ts
+++ src/app/pages/modules/home/playlist/playlist.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { ISong } from 'src/app/models/song.model';
+import { PlaylistProvider } from 'src/app/providers/playlist-provider';
 
 @Component({
   selector: 'app-playlist',
@@ -7,46 +9,12 @@ import { ISong } from 'src/app/models/song.model';
   styleUrls: ['./playlist.page.scss'],
 })
 export class PlaylistPage implements OnInit {
-
-  constructor() { }
+  playlistObservable: Observable<any>;
+  playlist:ISong[];
+  constructor(private playlistProvider :  PlaylistProvider) { }
 
   ngOnInit() {
+    this.playlistObservable = this.playlistProvider.getPlaylistObservable();
+    this.playlistObservable.subscribe(songs =>this.playlist = songs );
   }
-  playlist:ISong[] =[ 
-    {
-      name: "Baila conmigoooooo",
-      artists: ["Selena Gómez", "Rauw Alejandro"],
-      duration: "3:04",
-      photoURL:"https://pbs.twimg.com/media/Esru2SuWMAM_TD6.jpg:large",
-      path:"../../../../assets/tracks/Selena Gomez, Rauw Alejandro - Baila Conmigo (Official Video).m4a",
-      timesPlaying: 1458
-    },
-    {
-      name: "Bailaa conmigo",
-      artists: ["Selena Gómez", "Rauw Alejandro"],
-      duration: "3:04",
-      photoURL:"https://pbs.twimg.com/media/Esru2SuWMAM_TD6.jpg:large",
-      path:"../../../../assets/tracks/Selena Gomez, Rauw Alejandro - Baila Conmigo (Official Video).m4a",
-      timesPlaying: 2589
-
-    },
-    {
-      name: "Bailaa conmigo",
-      artists: ["Selena Gómez", "Rauw Alejandro"],
-      duration: "3:04",
-      photoURL:"https://pbs.twimg.com/media/Esru2SuWMAM_TD6.jpg:large",
-      path:"../../../../assets/tracks/Selena Gomez, Rauw Alejandro - Baila Conmigo (Official Video).m4a",
-      timesPlaying: 2589
-
-    },
-    {
-      name: "Bailaa conmigo",
-      artists: ["Selena Gómez", "Rauw Alejandro"],
-      duration: "3:04",
-      photoURL:"https://pbs.twimg.com/media/Esru2SuWMAM_TD6.jpg:large",
-      path:"../../../../assets/tracks/Selena Gomez, Rauw Alejandro - Baila Conmigo (Official Video).m4a",
-      timesPlaying: 2589
-
-    }
-  ];
 }
